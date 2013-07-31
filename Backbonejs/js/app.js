5d267f77bab533b9e22203b6f70f0dd359c3924e
Wouter Hemeryck <wouter@johnnybegood.be>
2013-07-31 15:19:24 +0200

    backbone skeleton

---
+++ Backbonejs/js/app.js
@@ -0,0 +1,27 @@
+﻿requirejs.config({
+    "baseUrl": "js",
+    "paths": {
+        "jquery": "//code.jquery.com/jquery"
+    },
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        bootstrap: { deps: ['jquery'] },
+        backbone: {
+            deps: [
+				'underscore',
+				'jquery'
+            ],
+            exports: 'Backbone'
+        },
+    }
+});
+
+require([
+    "views/master",
+    "bootstrap"
+], function (masterView, bootstrap) {
+    // this is where all the site code should begin
+    new masterView().render();
+});
---
+++ Backbonejs/js/views/products.js
@@ -0,0 +1,21 @@
+﻿define([
+        "jquery",
+        "underscore",
+        "backbone",
+], function ($, _, backbone) {
+    "use strict";
+
+    var masterView = backbone.View.extend({
+        initialize: function () {
+
+        },
+
+        render: function () {
+            this.$el.html("<h2>Products</h2>");
+            return this;
+        },
+
+    });
+
+    return masterView;
+});
