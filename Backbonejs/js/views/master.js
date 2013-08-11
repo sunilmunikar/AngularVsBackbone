159ca51e5d523be8e6efa97e1ced33df69594ff3
Wouter Hemeryck <wouter@johnnybegood.be>
2013-08-11 11:38:39 +0200

    master view

---
+++ Backbonejs/js/views/master.js
@@ -0,0 +1,27 @@
+﻿define([
+        "jquery",
+        "underscore",
+        "backbone",
+        "views/products",
+        "views/cart"
+], function ($, _, backbone, productsView, cartView) {
+    "use strict";
+
+    var masterView = backbone.View.extend({
+
+        el: "#app",
+
+        initialize: function () {
+            this.productsView = new productsView();
+            this.cartView = new cartView();
+        },
+
+        render: function () {
+            this.$el.find("#productspane").html(this.productsView.render().el);
+            this.$el.find("#cartpane").html(this.cartView.render().el);
+        },
+
+    });
+
+    return masterView;
+});
